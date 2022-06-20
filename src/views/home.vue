e6c97b64844881b78ba2a76120e695b60e3e4c8e
root <admin@example.com>
2022-06-20 18:46:27 +0300

    fix: (footer to botom, add sqrt home page)

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="main container max-w-7xl mx-auto p-3">
-        <img class="header__logoTree h-96 mx-auto z-10" src="/src/assets/tree.png" alt="WebDev">
+    <div class="main container max-w-7xl mx-auto p-3 flex flex-col items-center md:flex-row justify-around ">
+        <img class="header__logoTree w-40 h-auto z-10" src="/src/assets/tree.png" alt="WebDev">
+        <aside class="gradient-border min-w-[280px] min-h-[300px]"></aside>
     </div>
 </template>
+
+<style>
+.gradient-border {
+    border: solid;
+    border-image-slice: 1;
+    border-width: 5px;
+    border-image-source: linear-gradient(60deg,
+            #b16ec4,
+            #5f97b9);
+
+}
+</style>
