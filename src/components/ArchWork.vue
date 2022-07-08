1639328291f7b86693b2906abf7e4870a17fc990
root <admin@example.com>
2022-07-09 00:03:02 +0300

    fix: (galery)

--- src/components/ArchWork.vue
+++ src/components/ArchWork.vue
@@ -1,78 +1,78 @@
 <template>
     <h2 class="text-2xl text-center">Simple image gallery</h2>
     <!-- Photo Grid -->
-    <div class="archwork row" @click="showImghArchWork" ref="archwork">
+    <div class="archwork row after:dark:to-slate-900" @click="showImghArchWork" ref="archwork">
         <div class="column">
-            <img src="/src/assets/arch_work/1.jpg">
-            <img src="/src/assets/arch_work/2.jpg">
-            <img src="/src/assets/arch_work/3.jpg">
-            <img src="/src/assets/arch_work/4.jpg">
-            <img src="/src/assets/arch_work/5.jpg">
-            <img src="/src/assets/arch_work/6.jpg">
-            <img src="/src/assets/arch_work/7.jpg">
-            <img src="/src/assets/arch_work/8.jpg">
-            <img src="/src/assets/arch_work/9.jpg">
-            <img src="/src/assets/arch_work/10.jpg">
-            <img src="/src/assets/arch_work/11.jpg">
-            <img src="/src/assets/arch_work/12.jpg">
-            <img src="/src/assets/arch_work/13.jpg">
-            <img src="/src/assets/arch_work/14.jpg">
-            <img src="/src/assets/arch_work/15.jpg">
-            <img src="/src/assets/arch_work/16.jpg">
+            <img src="/src/assets/arch_work/1.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/2.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/3.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/4.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/5.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/6.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/7.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/8.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/9.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/10.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/11.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/12.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/13.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/14.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/15.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/16.jpg" loading="lazy" alt="Architect">
         </div>
         <div class="column">
-            <img src="/src/assets/arch_work/17.jpg">
-            <img src="/src/assets/arch_work/18.jpg">
-            <img src="/src/assets/arch_work/19.jpg">
-            <img src="/src/assets/arch_work/20.jpg">
-            <img src="/src/assets/arch_work/21.jpg">
-            <img src="/src/assets/arch_work/22.jpg">
-            <img src="/src/assets/arch_work/23.jpg">
-            <img src="/src/assets/arch_work/24.jpg">
-            <img src="/src/assets/arch_work/25.jpg">
-            <img src="/src/assets/arch_work/26.jpg">
-            <img src="/src/assets/arch_work/27.jpg">
-            <img src="/src/assets/arch_work/28.jpg">
-            <img src="/src/assets/arch_work/29.jpg">
-            <img src="/src/assets/arch_work/30.jpg">
-            <img src="/src/assets/arch_work/31.jpg">
-            <img src="/src/assets/arch_work/32.jpg">
+            <img src="/src/assets/arch_work/17.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/18.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/19.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/20.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/21.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/22.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/23.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/24.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/25.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/26.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/27.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/28.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/29.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/30.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/31.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/32.jpg" loading="lazy" alt="Architect">
         </div>
         <div class="column">
-            <img src="/src/assets/arch_work/33.jpg">
-            <img src="/src/assets/arch_work/34.jpg">
-            <img src="/src/assets/arch_work/35.jpg">
-            <img src="/src/assets/arch_work/36.jpg">
-            <img src="/src/assets/arch_work/37.jpg">
-            <img src="/src/assets/arch_work/38.jpg">
-            <img src="/src/assets/arch_work/39.jpg">
-            <img src="/src/assets/arch_work/40.jpg">
-            <img src="/src/assets/arch_work/41.jpg">
-            <img src="/src/assets/arch_work/42.jpg">
-            <img src="/src/assets/arch_work/43.jpg">
-            <img src="/src/assets/arch_work/44.jpg">
-            <img src="/src/assets/arch_work/45.jpg">
-            <img src="/src/assets/arch_work/46.jpg">
-            <img src="/src/assets/arch_work/47.jpg">
-            <img src="/src/assets/arch_work/48.jpg">
+            <img src="/src/assets/arch_work/33.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/34.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/35.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/36.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/37.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/38.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/39.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/40.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/41.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/42.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/43.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/44.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/45.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/46.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/47.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/48.jpg" loading="lazy" alt="Architect">
         </div>
         <div class="column">
-            <img src="/src/assets/arch_work/49.jpg">
-            <img src="/src/assets/arch_work/50.jpg">
-            <img src="/src/assets/arch_work/51.jpg">
-            <img src="/src/assets/arch_work/52.jpg">
-            <img src="/src/assets/arch_work/53.jpg">
-            <img src="/src/assets/arch_work/54.jpg">
-            <img src="/src/assets/arch_work/55.jpg">
-            <img src="/src/assets/arch_work/56.jpg">
-            <img src="/src/assets/arch_work/57.jpg">
-            <img src="/src/assets/arch_work/58.jpg">
-            <img src="/src/assets/arch_work/59.jpg">
-            <img src="/src/assets/arch_work/60.jpg">
-            <img src="/src/assets/arch_work/61.jpg">
-            <img src="/src/assets/arch_work/62.jpg">
-            <img src="/src/assets/arch_work/63.jpg">
-            <img src="/src/assets/arch_work/64.jpg">
+            <img src="/src/assets/arch_work/49.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/50.jpg" alt="Architect">
+            <img src="/src/assets/arch_work/51.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/52.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/53.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/54.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/55.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/56.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/57.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/58.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/59.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/60.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/61.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/62.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/63.jpg" loading="lazy" alt="Architect">
+            <img src="/src/assets/arch_work/64.jpg" loading="lazy" alt="Architect">
         </div>
     </div>
     <button @click="showMore"
@@ -84,39 +84,28 @@
 <style>
 .archwork.row {
     display: -ms-flexbox;
-    /* IE10 */
     display: flex;
     -ms-flex-wrap: wrap;
-    /* IE10 */
     flex-wrap: wrap;
     padding: 0 4px;
     position: relative;
     height: 600px;
     overflow: hidden;
-    background-color: white;
 }
 
-/* Create four equal columns that sits next to each other */
 .archwork .column {
     -ms-flex: 25%;
-    /* IE10 */
     flex: 25%;
     max-width: 25%;
     padding: 0 4px;
 }
 
 .archwork .column img {
-    margin-top: 8px;
-    vertical-align: middle;
-    width: 100%;
-    cursor: pointer;
-    @apply border-2 border-transparent;
+    @apply cursor-pointer w-full mt-2 align-middle;
 }
 
 .archwork .column img:not(.archworkImgFull):hover {
-    opacity: 0.5;
-    border: 2px solid red;
-    @apply border-teal-500;
+    filter: brightness(0.4);
 }
 
 .archworkBg:before {
@@ -126,29 +115,18 @@
     height: 100%;
     width: 100%;
     background-color: black;
-    opacity: 0.5;
+    opacity: 0.8;
 }
 
 .archwork:after {
     content: "";
-    display: block;
-    position: absolute;
-    height: 150px;
-    width: 100%;
-    bottom: 0;
-    background: linear-gradient(transparent, white);
+    @apply bg-gradient-to-b from-transparent to-gray-100 absolute h-36 w-full bottom-0 block;
 }
 
 .archworkImgFull {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 80% !important;
-    margin: 0 auto;
-    @apply border-4 border-teal-500;
+    @apply absolute inset-x-0 !w-4/5 mx-auto rounded-xl;
 }
 
-/* Responsive layout - makes a two column-layout instead of four columns */
 @media screen and (max-width: 800px) {
     .archwork .column {
         -ms-flex: 50%;
@@ -157,7 +135,6 @@
     }
 }
 
-/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
 @media screen and (max-width: 600px) {
     .archwork .column {
         -ms-flex: 100%;
